51ba6f0d77d459ea60c9a3f5c229762716cb03d6
ZtRXR <Zengtudor@outlook.com>
2023-08-02 18:00:57 +0800

    提交页面前端部分完成

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,21 +1,42 @@
+<script setup>
+import api from '../../HttpA';
+import {ref} from 'vue'
+
+let AUser = ref({
+    name: '',
+    pwd: ''
+})
+
+</script>
+
 <template>
     <div class="outCard">
         <el-card class="box-card">
-        <template #header>
-            <div class="card-header">
-                <span>请 登录 或者 注册</span>
-                <!-- <el-button class="button" text>Operation button</el-button> -->
-            </div>
-        </template>
-    </el-card>
+            <template #header>
+                <div class="card-header">
+                    <span>请 登录 或者 注册</span>
+                    <!-- <el-button class="button" text>Operation button</el-button> -->
+                </div>
+            </template>
+            <el-form :model="AUser">
+                <el-form-item label="账号名">
+                    <el-input v-model="AUser.name" placeholder="请输入用户名"></el-input>
+                </el-form-item>
+                <el-form-item label="密码">
+                    <el-input v-model="AUser.pwd" placeholder="请输入密码"></el-input>
+                </el-form-item>
+                <el-button type="primary">登录/注册</el-button>
+            </el-form>
+        </el-card>
     </div>
 </template>
   
 <style scoped>
-.outCard{
+.outCard {
     display: flex;
     justify-content: center;
 }
+
 .card-header {
     display: flex;
     justify-content: space-between;
